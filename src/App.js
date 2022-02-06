cc579317856dd7ff5257adbb87c224ae6c922d14
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2022-02-06 00:29:51 -0500

    Adds F1Garage function

--- src/App.js
+++ src/App.js
@@ -53,9 +53,32 @@ function MadeGoal() {
 function Goal(props) {
   const isGoal = props.isGoal;
   if (isGoal)
-    return <MadeGoal/>;
+    return <MadeGoal />;
   else
-    return <MissedGoal/>;
+    return <MissedGoal />;
+}
+
+function F1Garage() {
+  const cars = [
+    { position: 1, brand: 'Mercedes' },
+    { position: 2, brand: 'Redbull' },
+    { position: 3, brand: 'Ferrari' },
+    { position: 4, brand: 'McLaren' },
+    { position: 5, brand: 'Alpine' },
+    { position: 6, brand: 'AlphaTauri' },
+    { position: 7, brand: 'Aston Martin' },
+    { position: 8, brand: 'Williams' },
+    { position: 9, brand: 'Alfa Romeo' },
+    { position: 10, brand: 'Haas' },
+  ];
+  return (
+    <div>
+      <h2>F1 2021 standing</h2>
+      <ol>
+        {cars.map((car) => <li>{car.brand} is in position {car.position}</li>)}
+      </ol>
+    </div>
+  );
 }
 
 function App() {
@@ -77,6 +100,8 @@ function App() {
       <Football />
       <br></br>
       <Goal isGoal={true} />
+      <br></br>
+      <F1Garage />
       <br></br>
     </div>
   );
